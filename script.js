ab8a459602e0f65895ace79eb5cfad5d2c1aa45c
Taylor Shen <shen.taylor@yahoo.com>
2022-04-12 23:03:56 -0700

    js update

--- script.js
+++ script.js
@@ -1,1 +1,22 @@
-console.log("linked");
+//fetch weather api
+var apiKey = "e20fecbf0338ae51754a28dfd83d0b33";
+var city = "seattle";
+var requestURL = "https://api.openweathermap.org/data/2.5/weather?q=" + city + "&appid=" + apiKey + "&units=imperial";
+var searchBtn = document.getElementById("submitInput");
+
+//text box searches for city
+    //display city temp, wind, humidity, uv index with current date
+    //display next 5 days forecast
+//search history cities listed and clickable
+
+function getWeather() {
+    fetch(requestURL)
+        .then(function (response){
+            return response.json();
+        })
+        .then(function (data){
+            console.log(data);
+        })
+}
+
+searchBtn.addEventListener("click", getWeather);
